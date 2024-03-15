95adcf975693f471d5cfa68d8bbd8d35443aa666
Vaibhav <vaibhav7895@gmail.com>
2024-03-15 19:26:33 +0530

    backend

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require("express");
-const connection  = require("./db");
+const app = express();
+const connection  = require("./config/db");
 const cors=require("cors")
 require("dotenv").config()
 
 const {taskRouter} = require("./routes/task");
-const app = express();
+
 app.use(cors())
 app.use(express.json());
 app.use("/taskRouter", taskRouter);
@@ -15,8 +16,10 @@ app.listen(process.env.PORT, async () => {
     try {
         await connection;
         console.log("Connected to the database");
-        console.log("Server is running on port 3000");
+        
     } catch (err) {
         console.error("Error connecting to the database:", err);
     }
+    console.log(`Server is running on port no ${process.env.PORT}`);
 });
+
